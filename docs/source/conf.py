54a19cf97951d4017bed31aad575cfda160897ea
Carlos Wanderley <63980759+lcricco@users.noreply.github.com>
2022-05-07 15:13:42 -0700

    c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,4 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 
-html_static_path = ['_static']
-html_logo = "logo.png"
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+latex_logo = '_static/logo.png'
