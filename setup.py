d67b6f15cf8848808cdb4ca72d58dd9057c5d622
Carlos Wanderley <63980759+lcricco@users.noreply.github.com>
2022-05-07 10:02:37 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='project_3',
+      version='POC',
+      url='https://github.com/Carlos Ricco/project_3.git',
+      author='Carlos Ricco',
+      packages=['src'],
+      zip_safe=False)
